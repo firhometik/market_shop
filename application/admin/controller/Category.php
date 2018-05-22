ed1de7b525d96b6e1ebd7c981319d80f11198538
lvxudong <lvxudong@abc.com>
2018-05-22 22:51:12 +0800

    生活分类完成

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -16,8 +16,58 @@ class Category extends Admin
 		// dump($category);die;
 		return view('getCategory',['category'=>$category,'cate'=>$cate]);
 	}
-	public function setCategory()
-	{
-		
+	public function setcategory(){
+		if (!request()->isPost()) {
+			$this -> error('请求失败');
+		}
+		$data = input('post.');
+		$validate = validate('Category');
+		if (!$validate->check($data)) {
+			$this -> error($validate->getError());
+		}
+		if (!empty($data['id'])) {
+			$re = model('Category') -> save($data,['id'=>intval($data['id'])]);
+			if (!$re) _outPut('201','修改失败');
+			else _outPut('200','修改成功');
+		}else{
+			if (model('Category') -> add($data) !==false ) 
+				_outPut('200','添加成功');
+			else	_outPut('201','添加失败');
+		}
+	}
+	public function listorder($id,$listorder){
+		$re = model('Category') -> save(['listorder'=>$listorder],['id'=>$id]);
+		if ($re) {
+			$this -> result($_SERVER['HTTP_REFERER'],1,'success');
+		}else{
+			$this -> result($_SERVER['HTTP_REFERER'],0,'更新失败');
+		}
+	}
+	public function status(){
+		$id = input('get.id');
+		$status = model('Category') -> where(['id'=>$id]) -> value('status');
+		if ($status == 1) {
+			$re = model('Category') -> save(['status'=>0],['id'=>$id]);	
+		}else{
+			$re = model('Category') -> save(['status'=>1],['id'=>$id]);
+		}
+		if ($re) {
+			$this -> redirect($_SERVER['HTTP_REFERER'],1,'success');
+		}else{
+			$this -> redirect($_SERVER['HTTP_REFERER'],0,'更新失败');
+		}
+	}
+	public function delcategory(){
+		$id = input('get.id');
+		$list = model('Category') ->where(['parent_id'=>$id]) -> find();
+		if ($list) {
+			$this -> error('请先删除下级栏目');
+		}
+		$re = model('Category') ->where(['id'=>$id]) ->delete();
+		if ($re) {
+			$this -> result($_SERVER['HTTP_REFERER'],1,'success');
+		}else{
+			$this -> result($_SERVER['HTTP_REFERER'],0,'删除失败');
+		}
 	}
 }
